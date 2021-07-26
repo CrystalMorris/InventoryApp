28fb2db2959747d3d76b1fab6931f3825f0b5a16
ifeanchi <stanley.enyinnaya21@gmail.com>
2021-07-26 13:04:19 -0500

    added route to get all item

--- app.js
+++ app.js
@@ -23,16 +23,23 @@ app.use(express.static('public'));
 
 seed();
 
-// GET all items
-app.get('/items', async (req, res) => {
-    const allItems= await Item.findAll()
-    res.render('items', {allItems});
+// // GET all items
+// app.get('/items', async (req, res) => {
+//     const allItems= await Item.findAll()
+//     res.render('items', {allItems});
+// })
+
+app.get('/item', async (req, res) => {
+	const oneItems = await Item.findAll()
+	res.json({oneItems})
 })
 
-// app.get('/sauces/:id', async (req, res) => {
-//     const sauce = await Sauce.findByPk(req.params.id)
-//     res.render('sauce', {sauce}); 
-// })
+
+app.get('/item/:id', async (req, res) => {
+	const oneItems = await Item.findByPk(req.params.id)
+	res.json({oneItems})
+})
+
 
 app.listen(PORT, () => {
     sequelize.sync({force: true});
