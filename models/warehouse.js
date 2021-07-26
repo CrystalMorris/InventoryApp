53232cd5259dd941e5940ca0fbd756abf612c970
CrystalMorris <l.seagypsy.l@gmail.com>
2021-07-26 10:56:56 -0500

    added warehouse model and tests

---
+++ models/warehouse.js
@@ -0,0 +1,16 @@
+const {Sequelize, DataTypes, Model} = require('sequelize')
+const {sequelize} = require('../db')
+
+
+class Warehouse extends Model {}
+
+Warehouse.init({
+    name: DataTypes.STRING,
+    location: DataTypes.STRING
+}, {
+    sequelize,
+    timestamps: false,
+});
+ 
+
+module.exports = {Warehouse};
---
+++ models/warehouse.test.js
@@ -0,0 +1,28 @@
+const {Warehouse} = require('./warehouse');
+const {sequelize} = require('../db');
+
+
+describe('Warehouse Model', () => {
+	beforeAll(async () => {
+		await sequelize.sync({force: true})
+	});
+	
+	
+	test('can create a Warehouse', async() => {	
+		const testWarehouse1 = await Warehouse.create({name: 'West End', location:'123 Westview St., Someplace, USA'})	
+		expect(testWarehouse1.name).toBe('West End')
+	})
+
+	test('warehouse has a locatinon', async() => {
+		const testWarehouse2 = await Warehouse.create({name: 'South Side', location:'123 Southview Ln., Someplace, USA'})	
+		expect(testWarehouse2.location).toBe('123 South view Ln., Someplace, USA')
+	})
+
+	test('warehouse is added to the database', async() => {		
+		const testWarehouse3 = await Warehouse.create({name: 'East End', location:'123 Eastview Ave., Someplace, USA'})	
+		const foundItem = await Warehouse.findByPk(testWarehouse3.id)
+		expect(foundItem.name).toBe('East End')
+	}) 
+
+
+})
