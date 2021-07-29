26ddfa4211a06580d30ff72cfad6d3297ff199b7
CrystalMorris <l.seagypsy.l@gmail.com>
2021-07-29 13:05:30 -0500

    updated delete route to properly redirect back to warehouse view

--- app.js
+++ app.js
@@ -75,11 +75,12 @@ app.get('/warehouses/:id', async (req, res) =>{
 app.delete('/item/:id/delete', async(req, res)=>{    
     const thisItem =await Item.findByPk(req.params.id)    
     const thisWarehouse = await  Warehouse.findByPk(thisItem.WarehouseId)
-    console.log("this is app.js:79 " +JSON.stringify(thisWarehouse))
     await Item.destroy({where: {id: req.params.id}});  
-        // console.log("this is app.js:79 " +JSON.stringify(thisWarehouse))
-        // res.redirect('/warehouses/'+ thisWarehouse.id)
-        res.json(thisWarehouse)
+    console.log("Item Deleted!!")
+    const currentItems = await Item.findAll({where: {WarehouseId : thisWarehouse.id}})
+    console.log("Current number of items in "+ thisWarehouse.name +": " +JSON.stringify(currentItems.length))
+//not ideal finish but it works for now
+    res.json(thisWarehouse)
     })
 
     
--- public/script.js
+++ public/script.js
@@ -16,7 +16,8 @@ const deleteBtn = document.getElementById('delete-btn')
 
 deleteBtn.addEventListener('click', async (e) => {
 	let id = deleteBtn.dataset.item;
+	let warehouseid = deleteBtn.dataset.warehouse
 	await fetch(`/item/${id}/delete`, {method : 'DELETE'})
-	// window.location.pathname ='/warehouses' 
-	window.history.go(-1)
+	window.location.pathname ='/warehouses/' + warehouseid
+
 })
